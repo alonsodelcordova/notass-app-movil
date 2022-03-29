3457ab3831e9f6de196489899142195ef30c5dd5
EdisonCR <cordovaroman98@gmail.com>
2022-03-29 16:32:24 -0500

    cambios

--- app/src/main/java/com/example/notasapp/activitys/ui/UbicacionFragment.kt
+++ app/src/main/java/com/example/notasapp/activitys/ui/UbicacionFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.notasapp.R
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -18,8 +17,9 @@ class UbicacionFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
 
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            //-5.179578121951747, -80.61878958753458
+        val sydney = LatLng(-5.179578121951747, -80.61878958753458)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Universidad Nacional de Piura"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
